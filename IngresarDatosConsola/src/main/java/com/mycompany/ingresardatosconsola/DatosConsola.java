59e5f44dbfb3266086f01cde6e90e7173b9f7b37
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-23 11:09:48 -0500

    feat ingreso de datos por consola COMPLETE
    ingreso de datos pro consola COMPLETE
    
    resolves: #
    see also: #

--- IngresarDatosConsola/src/main/java/com/mycompany/ingresardatosconsola/DatosConsola.java
+++ IngresarDatosConsola/src/main/java/com/mycompany/ingresardatosconsola/DatosConsola.java
@@ -3,6 +3,8 @@
  */
 package com.mycompany.ingresardatosconsola;
 
+import java.util.Scanner;
+
 /**
  *
  * @author brian
@@ -12,8 +14,20 @@ public class DatosConsola {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
+        //Ingreso de datos por teclado consola
+        /*Scanner entrada = new Scanner(System.in);
+                
+        int valor;
+        System.out.println("Ingresa el valor: ");
+        valor = entrada.nextInt();
+        System.out.println("tu numero es: " + valor);*/
+        
+        /* Cadenas */
+        Scanner entrada = new Scanner(System.in);
+        String cadena;
+        System.out.println("Escriba una Cadena: ");
+        cadena = entrada.next();
+        System.out.println("la cadena es: " + cadena);
     }
-    
 }
