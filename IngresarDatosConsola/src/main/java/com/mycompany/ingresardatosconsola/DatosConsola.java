729f9d13336417fef3ccbd4e79580fea6bf18af4
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-23 09:19:31 -0500

    feat ingreso de datos por consola
    ingreso de datos pro consola
    
    resolves: #
    see also: #

---
+++ IngresarDatosConsola/src/main/java/com/mycompany/ingresardatosconsola/DatosConsola.java
@@ -0,0 +1,19 @@
+/*
+ * Ingreso de datos pro consola
+ */
+package com.mycompany.ingresardatosconsola;
+
+/**
+ *
+ * @author brian
+ */
+public class DatosConsola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
