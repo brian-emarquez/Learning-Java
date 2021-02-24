923972e08e19212c9847aaba5cae11a4894863f3
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-24 11:45:59 -0500

    feat condicionales_II_switch_case
    condicionales_II_switch_case
    
    resolves: #
    see also: #

--- condicionales_II_switch_case/src/main/java/condicionales_II.java
+++ condicionales_II_switch_case/src/main/java/condicionales_II.java
@@ -1,7 +1,8 @@
+
+import java.util.Scanner;
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Condicioales II Swich Case
  */
 
 /**
@@ -14,7 +15,24 @@ public class condicionales_II {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Condicioanles II
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Sleccione una Opcion: \n1: Acceso\n2: Confguracion\n3: Ayuda");
+        int seleccione = entrada.nextInt();
+        switch(seleccione){
+            case 1: System.out.println("Seleccionaleste el Acceso");
+            break;
+            
+            case 2: System.out.println("Seleccionaleste Configuracion");
+            break;
+            
+            case 3: System.out.println("Seleccionaleste Ayuda");
+            break;
+            
+            default:
+            System.out.println("Opcion no Valida"); 
+            break;
+        }
     }
-    
 }
