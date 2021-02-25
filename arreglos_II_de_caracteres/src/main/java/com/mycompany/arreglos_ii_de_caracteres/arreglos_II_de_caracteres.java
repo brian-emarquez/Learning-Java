33d83ddcaccc37f67d1e6c6e7f40e257f6270b0e
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-25 10:45:42 -0500

    feat Arreglos de caracteres
    Arreglos de caracteres
    
    resolves: #
    see also: #

---
+++ arreglos_II_de_caracteres/src/main/java/com/mycompany/arreglos_ii_de_caracteres/arreglos_II_de_caracteres.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.arreglos_ii_de_caracteres;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author brian
+ */
+public class arreglos_II_de_caracteres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // arreglos_II_de_caracteres
+        String texto = JOptionPane.showInputDialog("Ingresa un texto: ");
+        int longitud = texto.length();
+        char []caracteres = new char [longitud]; // cadenas
+        int inverso=longitud;
+        for(int i=0; i<longitud; i++)
+        {
+            caracteres[i] = texto.charAt(inverso-1);
+            inverso--;
+        }
+        System.out.println(caracteres);
+    }
+    
+}
