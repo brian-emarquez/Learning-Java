706a734049644df5489fc3a70541a09a388774c9
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-27 19:20:55 -0500

    feat burble UPDATE
    burble UPDATE
    resolves: #
    see also: #

--- ordenamiento_burbuja/src/main/java/ordenamiento_burbuja.java
+++ ordenamiento_burbuja/src/main/java/ordenamiento_burbuja.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,6 +18,25 @@ public class ordenamiento_burbuja {
      */
     public static void main(String[] args) {
         // ordenamiento_burbuja
+        // 5.4.2.1.3
+        String texto=JOptionPane.showInputDialog("Ingresa Numeros");
+        String numerosEnTexto[]=texto.split(",");
+        int cantidad=numerosEnTexto.length;
+        int numeros[]= new int[cantidad], temp;
+        for(int i=0;i<cantidad;i++){
+            numeros[i]=Integer.parseInt( numerosEnTexto[i]);
+        }
+        for(int i=0; i<cantidad; i++){
+            for(int j=0; j<cantidad; i++){
+                if (numeros[j] > numeros[j+1]){
+                    temp = numeros[j] ;
+                    numeros[j] = numeros[j+1];
+                    numeros[j]= ]temp;
+                        
+                }
+            }
+        }
+        
     }
     
 }
