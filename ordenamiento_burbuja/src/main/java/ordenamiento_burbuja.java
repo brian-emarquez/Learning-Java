d2d741f6ccd59415b0ce9d8ec64235cd995a9975
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-27 19:45:18 -0500

    feat burble COMPLETE
    burble COMPLETE
    resolves: #
    see also: #

--- ordenamiento_burbuja/src/main/java/ordenamiento_burbuja.java
+++ ordenamiento_burbuja/src/main/java/ordenamiento_burbuja.java
@@ -16,27 +16,30 @@ public class ordenamiento_burbuja {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // ordenamiento_burbuja
-        // 5.4.2.1.3
-        String texto=JOptionPane.showInputDialog("Ingresa Numeros");
+       public static void main(String[] args) {
+        //5,4,2,1,3
+        String texto=JOptionPane.showInputDialog("Humano ingresa numero divididos por coma");
         String numerosEnTexto[]=texto.split(",");
         int cantidad=numerosEnTexto.length;
-        int numeros[]= new int[cantidad], temp;
+
+        int numeros[]=new int[cantidad],tem;
         for(int i=0;i<cantidad;i++){
-            numeros[i]=Integer.parseInt( numerosEnTexto[i]);
+                    numeros[i]=Integer.parseInt( numerosEnTexto[i]);
         }
-        for(int i=0; i<cantidad; i++){
-            for(int j=0; j<cantidad; i++){
-                if (numeros[j] > numeros[j+1]){
-                    temp = numeros[j] ;
-                    numeros[j] = numeros[j+1];
-                    numeros[j]= ]temp;
-                        
+
+        for(int i=0;i<(cantidad-1);i++){
+            for(int j=0;j<(cantidad-1);j++){
+                if(numeros[j]>numeros[j+1]){
+                    tem=numeros[j];
+                    numeros[j]=numeros[j+1];
+                    numeros[j+1]=tem;
                 }
             }
         }
-        
+
+        System.out.println("Humano aqui estan tus numeritos ordenados de forma acendente");
+        for(int i=0;i<cantidad;i++){
+            System.out.print(numeros[i]+",");
+        }
     }
-    
 }
