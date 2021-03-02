2ac3ab7d8ad471cd02fabcbcf5e24d4e461fb2b2
Brian Marquez <brian3marquez@gmail.com>
2021-03-01 21:55:11 -0500

    feat ordenamiento pro seleccion
    Ordenamiento por seleccion
    
    see also:#
    resolves:#

--- ordenamiento_porSeleccion/src/App.java
+++ ordenamiento_porSeleccion/src/App.java
@@ -5,23 +5,37 @@ import javax.swing.text.Position;
 /* Ordenamiento por seleccion */
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // 5, 4, 2, 1, 3
 
-        Scanner entrada = new Scanner(System.in);
-        int elementos[] = new int[5];
-        int menor, pod, tem;
-        System.out.println("Escrbe lo numero par ordenar: ");
+        Scanner entra = new Scanner(System.in);
+        int elementos[] = new int[5], menor, pos, tem;
+        System.out.println("Escrbe los numero par ordenar: ");
         for (int i= 0; i<5; i++){
-            elementos[i] = entrada.nextInt();
+            elementos[i] = entra.nextInt();
         }
+
         for(int i=0; i<elementos.length-1; i++){
             menor = elementos[i];
-            for(int j=i+1; j<elementos.lenght-1; j++){
+            pos = i;
+            for(int j=i+1; j<elementos.length; j++){
+                if(elementos[j] < menor){
+                    menor = elementos[j];
+                    pos = j;
+                }
+            }
 
+            if(pos != i){
+                tem = elementos[i];
+                elementos [i] = elementos [pos];
+                elementos [pos]  = tem;
             }
         }
 
-
+        System.out.println("los elementos ordenas son: ");
+        for(int i=0; i<elementos.length; i++){
+            System.out.print(elementos[i] + ",");
+        }
     }
 }
+
