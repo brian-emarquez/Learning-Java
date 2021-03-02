18f5aa9ee352a7cead22154048074d584f1194de
brianMarquez3 <brian3marquez@gmail.com>
2021-03-01 20:17:10 -0500

    Feat Ordenamiento por Seleccion UPDATE
    Ordenamiento por Seleccion UPDATE
    
    Resolves: #
    see also: #

--- ordenamiento_porSeleccion/src/App.java
+++ ordenamiento_porSeleccion/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
+import javax.swing.text.Position;
+
+/* Ordenamiento por seleccion */
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // 5, 4, 2, 1, 3
+
+        Scanner entrada = new Scanner(System.in);
+        int elementos[] = new int[5];
+        int menor, pod, tem;
+        System.out.println("Escrbe lo numero par ordenar: ");
+        for (int i= 0; i<5; i++){
+            elementos[i] = entrada.nextInt();
+        }
+        for(int i=0; i<elementos.length-1; i++){
+            menor = elementos[i];
+            for(int j=i+1; j<elementos.lenght-1; j++){
+
+            }
+        }
+
+
     }
 }
