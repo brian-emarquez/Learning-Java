a8e0c417f65e05a4d47c1703314ac7bb8b522860
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-08 13:03:47 -0500

    Busqueda binaria COMPLETE
    Busqueda binaria COMPLETE
    
    See Also: #
    resolves:#

---
+++ Busqueda_Binaria/readme.md
@@ -0,0 +1,7 @@
+### Busqueda Binaria
+
+- Mas eficiente que la busqueda secuencial
+
+![java](./images/binaria.png)
+
+- Dividimos a la Mitad, se busca en la miatad divida
--- Busqueda_Binaria/src/main/java/busquedaBinaria.java
+++ Busqueda_Binaria/src/main/java/busquedaBinaria.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,6 +18,37 @@ public class busquedaBinaria {
      */
     public static void main(String[] args) {
         // Busqueda Binaria
+        // 5 10 15 30 50 70 90 95
+        Scanner entra = new Scanner(System.in);
+        int elementos[] = new int[8], menor, pos, tem;
+        System.out.println("Escrbe 8 numeros: ");
+        for (int i= 0; i<8; i++){
+            elementos[i] = entra.nextInt();
+        }
+        System.err.println("Ingresa un Numero para saber su posicion: ");
+        int numBuscar= entra.nextInt();
+        
+        int n = elementos.length, inf=0, centro=0;
+        int sup=n-1;
+        boolean bandera = false;
+        while(inf<=sup){
+            centro=(sup+inf)/2;
+            if(elementos [centro] == numBuscar){
+                bandera = true;
+            break;
+            }
+            else if (numBuscar < elementos[centro]){
+                    sup=centro-1;           
+            }
+             else{
+                inf= centro +1;
+            }
+        }
+        if (bandera == true) {
+        System.out.print(" Se encontro tu numero:" +(centro+1));
+       
+        }else{
+            System.out.print(" No se encontro tu numero");
+        }
     }
-    
 }
