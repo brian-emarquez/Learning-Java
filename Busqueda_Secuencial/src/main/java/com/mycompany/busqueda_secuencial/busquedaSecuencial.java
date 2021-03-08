842a4322763c0a20d6444b2c18ca8bc617f0abda
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-08 11:52:23 -0500

    Busqueda Secuandial START
    Biqueda Secuencial START
    
    See Also: #
    resolves:#

--- Busqueda_Secuencial/src/main/java/com/mycompany/busqueda_secuencial/busquedaSecuencial.java
+++ Busqueda_Secuencial/src/main/java/com/mycompany/busqueda_secuencial/busquedaSecuencial.java
@@ -3,18 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.mycompany.busqueda_secuencial;
 
 /**
  *
  * @author brian
  */
-public class ordenamientoSecuencial {
+public class busquedaSecuencial {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Ordenamiento Secuencial
+        // Busqueda Secuencial
     }
     
 }
