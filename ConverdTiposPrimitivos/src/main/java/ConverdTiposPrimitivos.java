30e443075981f2af2a10ee19c6b913c16e8ee1b6
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-16 17:02:27 -0500

    Update

---
+++ ConverdTiposPrimitivos/src/main/java/ConverdTiposPrimitivos.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+/*
+ Conversión de Tipos Primitivos
+ */
+
+/**
+ *
+ * @author brian
+ */
+public class ConverdTiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Conversión de Tipos Primitivos
+        
+        var edad = Integer.parseInt("20"); //covierte a numero entero
+        System.out.println("edad = " + (edad + 1));
+        
+        var valorPI = Double.parseDouble("3.141516");
+         System.out.println("valorPI = " + valorPI);
+         
+         //pedir un valor
+        var consola = new Scanner(System.in);
+        System.out.println("Proporciona tu edad:");
+        edad = Integer.parseInt(consola.nextLine());
+        System.out.println("edad = " + edad);
+         
+         
+    }
+}
