4c559d23ab50e7a0287df088bdfb0381754e7ba8
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-24 14:45:14 -0500

    Creacion de Metodos en JAVA
    Creacion de Metodos en Java
    
    see also:
    resolves:

---
+++ Creacion_Metodos_ Java/src/operaciones/Arimetica.java
@@ -0,0 +1,20 @@
+/*
+ * Creacion Metodos  Java
+ */
+package operaciones;
+
+/**
+ *
+ * @author brian
+ */
+public class Arimetica { //*siempre las clases empiesa con mayuscula*/
+    // Atributos de la clase
+    int a;
+    int b;
+    
+    //Metodo
+    public void sumar(String[] args) {
+        
+    }
+    
+}
