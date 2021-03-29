e01b8f0b6ae213777afadf83057da8e04c6fad05
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-29 12:07:32 -0500

    Proyecto Caja COMPLETE
    Proyecto Caja COMPLETE
    
    See also:
    resolves:

--- Proyecto_Caja/src/proyecto_caja/Proyecto_Caja.java
+++ Proyecto_Caja/src/proyecto_caja/Proyecto_Caja.java
@@ -15,7 +15,20 @@ public class Proyecto_Caja {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Proyecto caja
+        int medidaAncho = 3;
+        int medidaAlto = 2;
+        int medidaProf = 6;
+   
+        Caja caja1 = new Caja();
+        caja1.alto = medidaAlto;
+        caja1.profundo = medidaProf;
+        int resultado = caja1.calcularVolumen();
+    
+        System.out.println("resultado volumen de caja 1:" + resultado);
+
+        Caja caja2 = new Caja(2, 4, 6);
+        System.out.println("resultado volumen de caja 2:" + caja2.calcularVolumen());
     }
     
 }
