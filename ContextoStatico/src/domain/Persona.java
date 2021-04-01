b70727939b35ad74fc05a812f776a31dccb1af40
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-01 17:33:23 -0500

    Contexto Estatico
    Contexto Estatico
    
    see also:
    resolves:

--- ContextoStatico/src/domain/Persona.java
+++ ContextoStatico/src/domain/Persona.java
@@ -10,5 +10,39 @@ package domain;
  * @author brian
  */
 public class Persona {
-    
+
+    private int idPersona;
+    private String nombre; // static: se asocia con la clase y no con lo objetos
+    private static int contadorPersonas; // static: se asocia con la clase y no con lo objetos
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+
+        Persona.contadorPersonas++;
+//      contadorPersonas++;
+//      asignar el nuevo valor de la variable idPersona
+
+        this.idPersona = Persona.contadorPersonas;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
+    }
 }
