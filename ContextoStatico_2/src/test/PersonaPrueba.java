20a00f2989537c7aa750199defdb16d806850bd8
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-05 10:42:15 -0500

    CONTEXTO STATICO 2
    CONTEXTO STATICO 2
    
    see also:
    resolves:

--- ContextoStatico_2/src/test/PersonaPrueba.java
+++ ContextoStatico_2/src/test/PersonaPrueba.java
@@ -12,6 +12,8 @@ import domain.Persona;
  * @author brian
  */
 public class PersonaPrueba {
+    private int contador;
+    
     public static void main(String[] args) {
         Persona persona1 = new Persona("Brian");
         Persona persona2 = new Persona("Carla");
@@ -20,7 +22,13 @@ public class PersonaPrueba {
         imprimir(persona2);
     }
     
+    //static public  void imprimir(Persona persona //lo mismo
     public static void imprimir(Persona persona){ // tiene que se statico para llamar a la funcion imprimir
         System.out.println("persona = " + persona);   
+                 
+    }  
+    public int getContador(){  // con este metodo tenermos acceso
+        imprimir(new Persona("color"));
+        return this.contador = 10;
     }   
 }
