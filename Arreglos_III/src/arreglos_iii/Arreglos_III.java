3ad612d50754d035360b768394246f927f82b06e
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-20 13:45:34 -0500

    Array III

---
+++ Arreglos_III/src/arreglos_iii/Arreglos_III.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos_iii;
+
+/**
+ *
+ * @author brian
+ */
+public class Arreglos_III {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edades[] = new int [3];
+        System.out.println("edades = " + edades);
+        
+        edades[0] = 10;
+        System.out.println("edades o  = " + edades[0] );
+        
+        
+       
+    }
+    
+}
