46e0cf3356a06096aa975e5590b473b49a67e200
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-21 13:25:02 -0500

    Matrices 4

--- Matrices_IV/src/test/TestMatrices.java
+++ Matrices_IV/src/test/TestMatrices.java
@@ -5,6 +5,8 @@
  */
 package test;
 
+import domain.Persona;
+
 /**
  *
  * @author brian
@@ -18,20 +20,28 @@ public class TestMatrices {
         edades [0][1] = 7;
         edades [1][0] = 8;
         edades [1][1] = 4;
-        
-//        System.out.println("edades 0 - 0 = " + edades[0][0]);
-//        System.out.println("edades 0 - 0 = " + edades[0][1]);
 
         for (int ren = 0; ren < edades.length; ren++) {
                for (int col = 0; col < edades[ren].length; col++) {
                    System.out.println("edades = " + ren + "-" + col + ": " + edades[ren][col]);             
             }
-        }
+        }     
+        
         System.out.println("_______________________________________________");
         String frutas [][] = {{"Naranja, limon"},{"freasa", "Zarzamora"}};
-        for (int ren = 0; ren < frutas.length; ren++) {
-            for (int col = 0; col < frutas[ren].length; col++) {
-                System.out.println("frutas " + ren + "-" + col + ": " + frutas[ren][col]);
+        imprimir(frutas);
+        
+        Persona personas[][] = new Persona[2][3];/*al igual que lo arreglos se recomienda que sean en plural*/
+        personas [0][0] = new Persona("Juan");
+        personas [0][1] = new Persona("Karla");
+        imprimir(personas);
+    }
+    
+    public static void imprimir(Object matriz[][]){
+        
+        for (int ren = 0; ren < matriz.length; ren++) {
+            for (int col = 0; col < matriz[ren].length; col++) {
+                System.out.println("frutas " + ren + "-" + col + ": " + matriz[ren][col]);
             }
         }
     }
