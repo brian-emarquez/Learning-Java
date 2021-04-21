3057875f47a97f4408b50beb4eb9ba482df46581
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-21 12:35:12 -0500

    Matrices 2

--- Matrices_II/src/test/TestMatrices.java
+++ Matrices_II/src/test/TestMatrices.java
@@ -7,7 +7,7 @@ package test;
 
 /**
  *
- * @author brian 
+ * @author brian
  */
 public class TestMatrices {
     public static void main(String[] args) {
@@ -17,12 +17,15 @@ public class TestMatrices {
         edades [0][0] = 5;
         edades [0][1] = 7;
         edades [1][0] = 8;
-        edades [1][1] = 3;
+        edades [1][1] = 4;
         
-        System.out.println("edades 0 - 0 = " + edades[0][0]);
-        System.out.println("edades 0 - 0 = " + edades[0][1]);
-        System.out.println("edades 0 - 0 = " + edades[1][0]);
-        System.out.println("edades 0 - 0 = " + edades[1][1]);
-        
+//        System.out.println("edades 0 - 0 = " + edades[0][0]);
+//        System.out.println("edades 0 - 0 = " + edades[0][1]);
+
+        for (int ren = 0; ren < edades.length; ren++) {
+               for (int col = 0; col < edades[ren].length; col++) {
+                   System.out.println("edades = " + ren + "-" + col + ": " + edades[ren][col]);             
+            }
+        }
     }
 }
