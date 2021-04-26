2930720108becee7a56db2568aedaf6e8a0baed9
BrianMarquez3 <brian3maquez@gmail.com>
2021-04-26 12:17:51 -0500

    Argumentos Variables en Java

---
+++ Enumaciones/src/enumacion/TestEnumeraciones.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumacion;
+
+
+public class TestEnumeraciones {
+    public static void main(String[] args) {
+        System.out.println("Dias1:" + Dias.LUNES);
+        indicarDiaSemana(Dias.LUNES);
+    }
+    
+    private static void indicarDiaSemana(Dias dias){
+        switch(dias){
+            case LUNES:
+                System.out.println("Primer dia de la semana");
+                break;
+            case MARTES:
+                System.out.println("Primer Segundo de la semana");
+                break;
+            case MIERCOLES:
+                System.out.println("Primer tercer de la semana");
+                break;
+            case JUEVES:
+                System.out.println("Primer Cuarto de la semana");
+                break;
+            case VIERNES:
+                System.out.println("Primer quinto de la semana");
+                break;                   
+        }
+    }
+}
