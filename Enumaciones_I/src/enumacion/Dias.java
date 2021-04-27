1edf628389684dded7d9e6979a7b4f998cb7c28a
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-27 12:16:17 -0500

    Enumeracion

---
+++ Enumaciones_I/src/enumacion/Dias.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumacion;
+
+
+/* para crear un aenumacion usamos enum*, LISTA DE CONSTANTES*/
+public enum Dias {
+    LUNES, 
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO
+}
+
