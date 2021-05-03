52309038fb64401713790776b44454c8b3d94b3d
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-03 15:36:51 -0500

    Polimorfismo Complete

--- Polimorfismo/src/test/TestSobreescritura.java
+++ Polimorfismo/src/test/TestSobreescritura.java
@@ -5,6 +5,19 @@ import domain.*;
 public class TestSobreescritura {
     public static void main(String[] args) {
         Empleado empleado = new Empleado("Juan", 5000);
-        System.out.println("empeado = " + empleado.obtenerDetalles());
+//        System.out.println("empeado = " + empleado.obtenerDetalles());
+        imprimir(empleado);
+
+        Gerente gerente = new Gerente("Karla", 10000, "Contabilidad");
+//        System.out.println("empleado = " + gerente.obtenerDetalles());
+
+        imprimir(gerente);
     }
+    
+    /*Concepto de polimorfismo*/
+    public static void imprimir(Empleado empleado){
+        String detalles = empleado.obtenerDetalles();
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+    }
+    
 }
