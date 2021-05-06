e45028d333e2c5861a205349eff085758606092b
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-06 13:34:39 -0500

    feat Update
    UPDATE
    
    see also:
    resolve:

--- Conversión _de_Objetos/src/test/TestConversion.java
+++ Conversión _de_Objetos/src/test/TestConversion.java
@@ -6,17 +6,15 @@ public class TestConversion {
 
     public static void main(String[] args) {
         Empleado empleado;
-
         empleado = new Escritor("Juan", 5000, TipoEscritura.CLASICO);
 //      System.out.println("empleado = " + empleado);
         System.out.print(empleado.obtenerDetalles());
-        
+
         /* Downcasting*/
 //        ((Escritor)empleado).getTipoEscritura();
-        
-        Escritor escritor = (Escritor)empleado;
+        Escritor escritor = (Escritor) empleado;
         escritor.getTipoEscritura();
-        
+
         Empleado empleado2 = escritor;
 //        empleado2.obtenerDetalles();
     }
