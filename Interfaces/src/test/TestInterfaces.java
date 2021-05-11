3f4167f171e72c36e6bc78ba9ed1a85f789fd5e3
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-11 16:47:26 -0500

    Interfaces UPDATE

--- Interfaces/src/test/TestInterfaces.java
+++ Interfaces/src/test/TestInterfaces.java
@@ -6,7 +6,10 @@ import accesodatos.ImplementacionMysql;
 public class TestInterfaces {
     public static void main(String[] args) {
          IAccesoDatos datos = new ImplementacionMysql();
-         datos.listar();
-         
+//         datos.listar();  
+         mostar(datos);
+    }
+    public static void mostar(IAccesoDatos datos){
+        datos.listar();
     }
 }
