d65b077adc514a8bd21f65febbb042e3c51f42e0
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-14 12:41:43 -0500

    Update Readme

--- ManejoExcepciones_II/src/test/TestExcepciones.java
+++ ManejoExcepciones_II/src/test/TestExcepciones.java
@@ -1,11 +1,14 @@
 /* manejo de errores */
 package test;
 
+import static aritmetica.Aritmetica.division;
+
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
+        
         try{
-            resultado = 10/0;
+            resultado = division(10,0 );
         }catch(Exception e){
             System.out.println("Ocurrió un error: ");
             e.printStackTrace(System.out);
