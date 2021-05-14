7b709032fef2fd01f5829b41007a9b007814cd69
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-14 18:45:27 -0500

    Manejo de Generics Update

--- ManejoGenerics/src/test/TestGenerics.java
+++ ManejoGenerics/src/test/TestGenerics.java
@@ -1,7 +1,10 @@
 package test;
 
+import genericos.ClaseGenerica;
+
 public class TestGenerics {
     public static void main(String[] args) {
-        
+        ClaseGenerica<Integer> objetoInt = new ClaseGenerica(15);
+        objetoInt.obtenerTipo();
     }
 }
