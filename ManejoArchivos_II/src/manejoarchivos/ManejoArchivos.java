c6c3490394040f6d037df6bc67d539c16dfb9356
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-14 22:03:03 -0500

    Manejo de Archivos - lectura

--- ManejoArchivos_II/src/manejoarchivos/ManejoArchivos.java
+++ ManejoArchivos_II/src/manejoarchivos/ManejoArchivos.java
@@ -1,10 +1,14 @@
 package manejoarchivos;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ManejoArchivos {
 
@@ -45,12 +49,32 @@ public class ManejoArchivos {
             PrintWriter salida = new PrintWriter(new FileWriter(archivo, true));
             salida.print(contenido);
             salida.close();
-            System.out.println("Se ha escrito en el archivo");
+            System.out.println("Se ha anexado en el archivo");
 
         } catch (FileNotFoundException ex) {
             ex.printStackTrace(System.out);
         } catch (IOException ex) {
             ex.printStackTrace(System.out);
         }
+    }
+
+    public static void leerArchivo(String nombreArchivo) {
+        var archivo = new File(nombreArchivo);
+        try {
+            //BufferredReader nos ayuda a leer archivos
+            var entrada = new BufferedReader(new FileReader(archivo));
+            var lectura = entrada.readLine();
+            while (lectura != null) {
+                System.out.println("lectura = " + lectura);
+                lectura = entrada.readLine();
+            }
+            entrada.close();
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+
     }
 }
