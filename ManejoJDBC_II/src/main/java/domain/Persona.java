93f33c5d3c9f64d15510116426cf01ae98fa047e
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-17 13:57:05 -0500

    feat manejo del JDBC UPDATE
    Manejo del JDBC UPDATE
    
    see also:
    resolves:

--- ManejoJDBC_II/src/main/java/domain/Persona.java
+++ ManejoJDBC_II/src/main/java/domain/Persona.java
@@ -1,4 +1,4 @@
-
+/* Se agregaron cnstructores, get an set*/
 package domain;
 
 
@@ -21,7 +21,49 @@ public class Persona {
         this.apellido = apellido;
         this.email = email;
     }
-    
-    
-    
+
+    public Persona(int id_Persona, String nombre, String apellido, String email) {
+        this.id_Persona = id_Persona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public int getId_Persona() {
+        return id_Persona;
+    }
+
+    public void setId_Persona(int id_Persona) {
+        this.id_Persona = id_Persona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id_Persona=" + id_Persona + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + '}';
+    }
+
 }
