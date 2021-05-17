6b0fc0fcddd00a21724f65fb15296a36ce531573
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-17 13:58:59 -0500

    feat manejo del JDBC III
    Manejo del JDBC III
    
    see also:
    resolves:

---
+++ ManejoJDBC_III/src/main/java/domain/Persona.java
@@ -0,0 +1,69 @@
+/* Se agregaron cnstructores, get an set*/
+package domain;
+
+
+public class Persona {
+    private int id_Persona;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public Persona() {
+        
+    }
+
+    public Persona(int id_Persona) {
+        this.id_Persona = id_Persona;
+    }
+
+    public Persona(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public Persona(int id_Persona, String nombre, String apellido, String email) {
+        this.id_Persona = id_Persona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public int getId_Persona() {
+        return id_Persona;
+    }
+
+    public void setId_Persona(int id_Persona) {
+        this.id_Persona = id_Persona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id_Persona=" + id_Persona + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + '}';
+    }
+
+}
---
+++ ManejoJDBC_III/src/main/java/test/TestMySQL.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.sql.*;
+
+public class TestMySQL {
+
+    public static void main(String[] args) {
+        var url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection(url, "root", "");
+            Statement instruccion = conexion.createStatement();
+            var sql = "SELECT id_persona, nombre, apellido, email FROM persona";
+            ResultSet resultado = instruccion.executeQuery(sql);
+            while (resultado.next()) { // next : si tenemos un objeto aun por iterar . Veraddero o falso
+                System.out.print("id_persona = " + resultado.getInt("id_persona"));
+                System.out.print(" Nombre = " + resultado.getString("nombre"));
+                System.out.print(" Apellido = " + resultado.getString("apellido"));
+
+                System.out.print(" | ");
+            }
+            resultado.close();
+            instruccion.close();
+            conexion.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
