b5075732c6735491f7be5277a89a1a4b8c19cdba
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-17 12:21:28 -0500

    MYsql Update

--- IntroduccionJDBCMySQL/src/main/java/test/TestMySQL.java
+++ IntroduccionJDBCMySQL/src/main/java/test/TestMySQL.java
@@ -1,24 +1,23 @@
-
 package test;
 
 import java.sql.*;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-
 public class TestMySQL {
+
     public static void main(String[] args) {
-        String url="jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        var url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
         try {
 //            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conexion = DriverManager.getConnection(url, "root", "");
             Statement instruccion = conexion.createStatement();
             var sql = "SELECT Nombre, Edad, Sexo FROM test";
             ResultSet resultado = instruccion.executeQuery(sql);
+            while (resultado.next()) { // next : si tenemos un objeto aun por iterar . Veraddero o falso
+                System.out.println("Persona = " + resultado.getInt("persona"));
+            }
             
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        }  
+        }
     }
 }
