3c3c7d28a207f431983d059d31f10bd4e0102176
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-26 12:20:42 -0500

    Hola Mundo Java Web

--- HolaMundoWeb/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.mycompany/HolaMundoWeb .
-call docker rm -f HolaMundoWeb
-call docker run -d -p 9080:9080 -p 9443:9443 --name HolaMundoWeb com.mycompany/HolaMundoWeb
