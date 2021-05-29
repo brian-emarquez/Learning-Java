451b57f24021a8ecf0f5ddc8ff56d59a140eee77
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-29 12:22:47 -0500

    Hola Mundo Spring

---
+++ holaMundoSpring/src/test/java/com/nightsroiud/holaMundo/HolaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nightsroiud.holaMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
