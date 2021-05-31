a0b35d54e4923d72647a9cef5cad6703f67b9bb0
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-31 11:48:41 -0500

    Se Agrego Javascrips

--- ManejoFormularioHTML/src/main/webapp/recursos/funciones.js
+++ ManejoFormularioHTML/src/main/webapp/recursos/funciones.js
@@ -1,7 +1,37 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+function validar(forma){
+   var usuario =  forma.usuario;
+   // validacion de usuario
+   if(usuario.value =="" || usuario.value == "Escribir usuario"){
+        alert("Debe proporcionar un nombre de usuario");
+        usuario.focus();
+        usuario.select();
+        return false;         
+   }
+   // validacion de password
+   var password = forma.password;
+   if(usuario.value =="" | usuario.value.length<3){
+        alert("Debe proporcionar un password al menor de 3 caracteres");
+        password.focus();
+        password.select();
+        return false;         
+   }
+   
+    // validacion de check tenologias 
+   var tecnologia = forma.tecnologia;
+   var checkseleccionado = false;
+   
+    for (var i = 0; i < tecnologia.length; i++) {
+        if(tecnologia[i].check){
+            checkseleccionado = true;
+        }
+    }
+    
+    if(!checkseleccionado){
+        alert("Debe seleccionar una tenologia")
+        return false;
+    }  
+    
+    
+ }
+   
+   
