f7556eba97f4f25d9c81efcf178c639229f30404
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-31 12:44:02 -0500

    Se Agrego Javascrips

--- ManejoFormularioHTML/src/main/java/web/Servlet.java
+++ ManejoFormularioHTML/src/main/java/web/Servlet.java
@@ -1,11 +1,24 @@
 package web;
 
 import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet{
     
-    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html:charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        // Recuperadno el parametro
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        String tecnologias[] = response.getParameterValues("tecnologias");
+        
+        
+    }
 }
--- ManejoFormularioHTML/src/main/webapp/recursos/funciones.js
+++ ManejoFormularioHTML/src/main/webapp/recursos/funciones.js
@@ -1,37 +1,55 @@
-function validar(forma){
-   var usuario =  forma.usuario;
-   // validacion de usuario
-   if(usuario.value =="" || usuario.value == "Escribir usuario"){
+function validarForma(forma){
+    var usuario = forma.usuario;
+    if(usuario.value == "" || usuario.value == "Escribir usuario"){
         alert("Debe proporcionar un nombre de usuario");
         usuario.focus();
         usuario.select();
-        return false;         
-   }
-   // validacion de password
-   var password = forma.password;
-   if(usuario.value =="" | usuario.value.length<3){
-        alert("Debe proporcionar un password al menor de 3 caracteres");
+        return false;
+    }
+    
+    var password = forma.password;
+    if(password.value == "" || password.value.length < 3){
+        alert("Debe proporcionar un password al menos de 3 caracteres");
         password.focus();
         password.select();
-        return false;         
-   }
-   
-    // validacion de check tenologias 
-   var tecnologia = forma.tecnologia;
-   var checkseleccionado = false;
-   
-    for (var i = 0; i < tecnologia.length; i++) {
-        if(tecnologia[i].check){
-            checkseleccionado = true;
+        return false;
+    }
+    
+    var tecnologias = forma.tecnologia;
+    var checkSeleccionado = false;
+    
+    for(var i = 0; i < tecnologias.length; i++){
+        if(tecnologias[i].checked){
+            checkSeleccionado = true;
         }
     }
     
-    if(!checkseleccionado){
-        alert("Debe seleccionar una tenologia")
+    if(!checkSeleccionado){
+        alert("Debe seleccionar una tecnología");
         return false;
-    }  
+    }
     
+    var generos = forma.genero;
+    var radioSeleccionado = false;
     
- }
-   
-   
+    for(var i = 0; i < generos.length; i++){
+        if(generos[i].checked){
+            radioSeleccionado = true;
+        }
+    }
+    
+    if(!radioSeleccionado){
+        alert("Debe seleccionar un genero");
+        return false;
+    }
+    
+    var ocupacion = forma.ocupacion;
+    if(ocupacion.value == ""){
+        alert("Debe seleccionar una ocupacion");
+        return false;
+    }
+    
+    //Formulario es valido
+    alert("Formulario valido, enviado datos al servidor");
+    return true;
+}
