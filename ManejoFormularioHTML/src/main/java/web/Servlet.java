a2c273abb9f4056b85b0222adb3cee4581fb5eab
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-31 13:03:59 -0500

    Manejo de FGormulario Servlet UODATE 2

--- ManejoFormularioHTML/src/main/java/web/Servlet.java
+++ ManejoFormularioHTML/src/main/java/web/Servlet.java
@@ -14,10 +14,32 @@ public class Servlet extends HttpServlet{
         response.setContentType("text/html:charset=UTF-8");
         PrintWriter out = response.getWriter();
         
-        // Recuperadno el parametro
+        // Recuperacion de parametros
         String usuario = request.getParameter("usuario");
         String password = request.getParameter("password");
-        String tecnologias[] = response.getParameterValues("tecnologias");
+        String tecnologias[] = request.getParameterValues("tecnologia");
+        String genero = request.getParameter("genero");
+        String ocupacion = request.getParameter("ocupacion");
+        String musica[] = request.getParameterValues("musica");
+        String Comentario = request.getParameter("comentario");
+
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title> Resultado del Servlet<title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<h1> Parametros pricesador pro el servlet</h1>");
+        out.print("<table border='1'>");       
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("usuario");
+        out.print("</td>");
+        out.print("</tr>");
+        out.print("<table>");
+        
+
+        
+        
         
         
     }
