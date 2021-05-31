343068a4c9bbbe6315cf4c99a26c2207a9f948f7
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-31 13:21:29 -0500

    Manejo de FGormulario Servlet UODATE 3

--- ManejoFormularioHTML/src/main/java/web/Servlet.java
+++ ManejoFormularioHTML/src/main/java/web/Servlet.java
@@ -1,46 +1,114 @@
 package web;
 
-import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/Servlet")
-public class Servlet extends HttpServlet{
-    
+public class Servlet extends HttpServlet {
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        response.setContentType("text/html:charset=UTF-8");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        
-        // Recuperacion de parametros
+
         String usuario = request.getParameter("usuario");
         String password = request.getParameter("password");
         String tecnologias[] = request.getParameterValues("tecnologia");
         String genero = request.getParameter("genero");
         String ocupacion = request.getParameter("ocupacion");
         String musica[] = request.getParameterValues("musica");
-        String Comentario = request.getParameter("comentario");
+        String comentarios = request.getParameter("comentarios");
 
         out.print("<html>");
         out.print("<head>");
-        out.print("<title> Resultado del Servlet<title>");
+        out.print("<title>Resultado Servlet</title>");
         out.print("</head>");
         out.print("<body>");
-        out.print("<h1> Parametros pricesador pro el servlet</h1>");
-        out.print("<table border='1'>");       
+        out.print("<h1>Parametros procesados por el Servlet:</h1>");
+        out.print("<table border='1'>");
+
         out.print("<tr>");
         out.print("<td>");
-        out.print("usuario");
+        out.print("Usuario");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(usuario);
         out.print("</td>");
         out.print("</tr>");
-        out.print("<table>");
-        
 
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Password");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(password);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Tecnologías");
+        out.print("</td>");
+        out.print("<td>");
+
+        for (String tecnologia : tecnologias) {
+            out.print(tecnologia);
+            out.print(" / ");
+        }
+
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Género");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(genero);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Ocupación");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(ocupacion);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Música Favorita");
+        out.print("</td>");
+        out.print("<td>");
+
+        if (musica != null) {
+            for (String m : musica) {
+                out.print(m);
+                out.print(" / ");
+            }
+        }
+        else{
+            out.print("Valor no proporcionado");
+        }
+
+        out.print("</td>");
+        out.print("</tr>");
         
-        
-        
-        
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Comentarios");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(comentarios);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("</table>");
+        out.print("</body>");
+        out.print("</html>");
     }
 }
