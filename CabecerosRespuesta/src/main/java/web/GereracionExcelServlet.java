58f952b942451833c7655a24bc6f44402eee4634
BrianMarquez3 <brian3marquez@gmail.com>
2021-06-02 11:32:48 -0500

    Cabeceros de Respuesta con Servlets EJERCICIO UPDATE 1

--- CabecerosRespuesta/src/main/java/web/GereracionExcelServlet.java
+++ CabecerosRespuesta/src/main/java/web/GereracionExcelServlet.java
@@ -1,5 +1,32 @@
 package web;
 
-public class GereracionExcelServlet {
-    
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/GeneracionExcelServlet")
+public class GereracionExcelServlet extends HttpServlet{
+    protected void doGet(HttpServletRequest resquest, HttpServletResponse response) throws IOException{
+        
+        /* Indicamos el tipo de respuesta al navegador*/
+        response.setContentType("applicacion/vnd.ms-excel"); // Informacion tipo excel
+        response.setHeader("Content-Disposition", "attachment;filename=excelEjemplo.xls");
+        /* par aun uso mas profesionas de escel pol.aparache.prg*/
+        
+        /*cabeceros - Limpiar cache*/
+        response.setHeader("pragma", "no-cache");
+        response.setHeader("Cache-control", "no-store");
+        response.setDateHeader("Expires", -1);
+        
+        /* Desplagamos la informacion al cliente */
+        PrintWriter out = response.getWriter();
+        out.print("\tValores");
+        out.print("\t1");
+        out.print("\t2");
+        out.print("Total\t=SUMA(b2:B3)");
+        out.close();
+    }
 }
--- CabecerosRespuesta/src/main/java/web/HoraServlet.java
+++ CabecerosRespuesta/src/main/java/web/HoraServlet.java
@@ -1,5 +1,22 @@
 package web;
 
-public class HoraServlet {
-    
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HoraServlet")
+public class HoraServlet extends HttpServlet{
+    protected void goGet(HttpServletRequest resquest, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        response.setHeader("refresh", "1");
+        Date fecha = new Date();
+        SimpleDateFormat formateador = new SimpleDateFormat("'Hora actializada' HH:mm:ss");
+        
+        
+        
+    }   
 }
