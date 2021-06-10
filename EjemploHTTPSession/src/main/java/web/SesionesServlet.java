4698f29f7e1acd3daf58500037fc1535952f265d
BrianMarquez3 <brian3marquez@gmail.com>
2021-06-09 21:31:41 -0500

    HTTP sesion UPDATE 2

--- EjemploHTTPSession/src/main/java/web/SesionesServlet.java
+++ EjemploHTTPSession/src/main/java/web/SesionesServlet.java
@@ -2,8 +2,31 @@ package web;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/SesionesServlet")
 public class SesionesServlet extends HttpServlet{
     
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        response.setContentType("text/html;charset=UT8");
+        
+        HttpSession sesion  = request.getSession();
+        String titulo = null;
+        
+        /*Pedir el atributo contador visitas a la sesion */
+        Integer contadorVisitas = (Integer)sesion.getAttribute("ContadorVisitas");
+        /* si es nulo es la  primera ves que estamos accedesmos a la aplicacion*/
+        if(contadorVisitas == null){
+            contadorVisitas = 1;
+            titulo = "Bienvenido por primera ves";      
+        }else{
+            contadorVisitas++;
+            titulo= "Bienvenido nuevamente";
+        } 
+        
+        
+    }
 }
