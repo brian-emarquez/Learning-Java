fb27b6cef3ad35476b43adbe404f1ba53d4d4717
BrianMarquez3 <brian3marquez@gmail.com>
2021-06-10 15:37:57 -0500

    Carrito de COmpras UPDATE

--- CarritoCompras/src/main/java/web/CarritoServlet.java
+++ CarritoCompras/src/main/java/web/CarritoServlet.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package web;
 
-/**
- *
- * @author brian
- */
-public class CarritoServlet {
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/CarritoServlet")
+public class CarritoServlet extends HttpServlet{
     
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        
+        //procesamos el nuevo articulo
+        String articuloNuevo = request.getParameter("articulo");
+        
+        // creamos o recuperamos el objeto
+        HttpSession sesion = request.getSession();
+                
+        //recuperamos la lista de articulos agregados prevoamente si esisten
+        List<String> articulos = (List<String>) sesion.getAttribute("articulos");
+                
+    }
+  
 }
