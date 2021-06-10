a6327aeed66eba70e5c6445506d9b197abe47e5f
BrianMarquez3 <brian3marquez@gmail.com>
2021-06-09 21:04:07 -0500

    HTTP sesion

---
+++ EjemploHTTPSession/src/main/java/web/SesionesServlet.java
@@ -0,0 +1,9 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/SesionesServlet")
+public class SesionesServlet extends HttpServlet{
+    
+}
