6fd5205f25af6146cd35470fdc1625d4f90f077e
BrianMarquez3 <brian3marquez@gmail.com>
2021-06-09 21:52:58 -0500

    HTTP sesion UPDATE 3

--- EjemploHTTPSession/src/main/java/web/SesionesServlet.java
+++ EjemploHTTPSession/src/main/java/web/SesionesServlet.java
@@ -1,5 +1,7 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +12,8 @@ import javax.servlet.http.HttpSession;
 public class SesionesServlet extends HttpServlet{
     
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response){
-        response.setContentType("text/html;charset=UT8");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTf-8");
         
         HttpSession sesion  = request.getSession();
         String titulo = null;
@@ -27,6 +29,17 @@ public class SesionesServlet extends HttpServlet{
             titulo= "Bienvenido nuevamente";
         } 
         
+        //Agregamos el nuevo valor a la sesion
+        sesion. setAttribute("ContadorVisitas", contadorVisitas);
         
+        // Mandamos la respuesl al cliente
+        PrintWriter out = response.getWriter();
+        out.print(titulo);
+        out.print("<br>");
+        out.print("Nro de accesos al recurso: " + contadorVisitas);
+        out.print("<br>");
+        out.print("ID de la sesion: " + sesion.getId());
+        
+        out.close();   
     }
 }
