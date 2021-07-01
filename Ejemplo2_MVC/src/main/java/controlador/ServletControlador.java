ffc9657a83b8fa36aa59aa5c2c8fec5c97058670
brian3marquez <brian3marquez@gmail.com>
2021-07-01 12:56:11 -0500

    EJEMPLO 2 MVC Update

--- Ejemplo2_MVC/src/main/java/controlador/ServletControlador.java
+++ Ejemplo2_MVC/src/main/java/controlador/ServletControlador.java
@@ -3,6 +3,7 @@ package controlador;
 import java.awt.Rectangle;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -13,17 +14,31 @@ public class ServletControlador extends HttpServlet{
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        
         //1. Procesamos parametros 
+        String accion = request.getParameter("accion");
         
         //2, Creamos los javaBeans
-        Rectangulo rec = new Rectangulo(3,6);
-        
+        Rectangulo recRequest = new Rectangulo(1,2);
+        Rectangulo recSesion = new Rectangulo(3,4);
+        Rectangulo recAplicacion = new Rectangulo(5,6);
+
         //3. Agregamos al javabeans aa algun alcance
-        request.setAttribute("mensaje", "Saludos desde el Servlet");
         
-        HttpSession sesion = request.getSession();
-        sesion.setAttribute("rectangulo", rec);
         
+        //Revisamos la accion proporcinada
+        if("agregarVariables".equals(accion)){
+            //Alcance request
+            request.setAttribute("rectanguloRequest", recRequest);
+            // Alcance session
+            HttpSession session = request.getSession();
+            session.setAttribute("rectanguloSession", request);
+            
+            //Alcanse apllication
+            ServletContext application = this.getServletContext();
+            application.setAttribute("mensaje", "Las variables fueron agregadas");
+        }
+                
         //4. recionar a la vista selecionada
         RequestDispatcher rd = request.getRequestDispatcher("vista/desplegarVariables.jsp");
         rd.forward(request, response);
