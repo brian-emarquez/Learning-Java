2efa04246e3a93444c2280ec0128123d3622910c
brian3marquez <brian3marquez@gmail.com>
2021-07-02 12:18:26 -0500

    UPDATE

---
+++ HolaMundoJSF/src/main/java/beans/configuracion/ConfigrationJSF.java
@@ -0,0 +1,12 @@
+package beans.configuracion;
+import javax.faces.annotation.FacesConfig;
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+
+@FacesConfig(
+    //Activa CDI- build-in beans
+    version = JSF_2_3            
+)
+
+public class ConfigrationJSF {
+    
+}
