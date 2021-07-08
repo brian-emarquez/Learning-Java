06c85264f0b2dd0613720b86cdedd6ae34cdab6f
BrianMarquez3 <brian3marquez@gmail.com>
2021-07-08 12:52:28 -0500

    Control Clientes Cliente

---
+++ ControlClientes/src/main/java/dominio/Cliente.java
@@ -0,0 +1,37 @@
+package dominio;
+
+public class Cliente {
+    
+    private int idCliente;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private double saldo;
+
+    public Cliente() {
+    }
+
+    public Cliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Cliente(String nombre, String apellido, String email, String telefono, double saldo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.saldo = saldo;
+    }
+
+    public Cliente(int idCliente, String nombre, String apellido, String email, String telefono, double saldo) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.saldo = saldo;
+    }
+    
+    
+}
