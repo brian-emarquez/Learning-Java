e58ece8e4598a43e22ae58c3057fb978645b3674
brian3marquez <brian3marquez@gmail.com>
2021-07-31 17:25:27 -0500

    Comtrol Clientes - CLIENTES

--- ControlClientes/src/main/java/web/ServletControlador.java
+++ ControlClientes/src/main/java/web/ServletControlador.java
@@ -1,5 +1,10 @@
 package web;
 
+import datos.ClienteDaoJDBC;
+import dominio.Cliente;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
@@ -7,8 +12,12 @@ import javax.servlet.http.*;
 public class ServletControlador extends HttpServlet{
    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response){
-        List<Cliente> clientes = 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException{
+        List<Cliente> clientes =  new ClienteDaoJDBC().listar();
+        System.out.println("clientes = " + clientes);
+        
+        request.setAttribute("clientes", clientes);
+        request.getRequestDispatcher("clientes.jsp").forward(request, response);
     }
-      
 }
