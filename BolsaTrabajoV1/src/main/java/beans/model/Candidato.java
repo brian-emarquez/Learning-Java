42040c6b59c7bfa57d928d7310060ae69beb3c53
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-29 21:06:57 -0500

    balsa de trabajo v1 update 2

--- BolsaTrabajoV1/src/main/java/beans/model/Candidato.java
+++ BolsaTrabajoV1/src/main/java/beans/model/Candidato.java
@@ -8,17 +8,18 @@ import org.apache.logging.log4j.Logger;
 @Named
 @RequestScoped
 public class Candidato {
+
     private String nombre;
     private String apellido;
     private String salarioDeseado;
-    
+
     Logger log = LogManager.getRootLogger();
-    
-    public Candidato(){
+
+    public Candidato() {
         log.info("Creando el objeto candidato");
         this.setNombre("Introduce tu nombre");
     }
-    
+
     public String getNombre() {
         return nombre;
     }
@@ -27,7 +28,24 @@ public class Candidato {
         this.nombre = nombre;
         log.info("MOdificando la propiedad de nombre " + this.nombre);
     }
-            
-            
-            
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+        log.info("Modificacndo la propiedad de apellido " + this.apellido);
+    }
+
+    public String getSalarioDeseado() {
+        return salarioDeseado;
+    }
+
+    public void setSalarioDeseado(String salarioDeseado) {
+        this.salarioDeseado = salarioDeseado;
+        log.info("Modificacndo la propiedad de sueldoDeseado " + this.salarioDeseado);
+
+    }
+
 }
