d86387f9a92f7871b4c692199f6286d51b5bd410
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-29 21:40:06 -0500

    balsa de trabajo v1 Complete

--- BolsaTrabajoV1/src/main/java/beans/backing/VacanteForm.java
+++ BolsaTrabajoV1/src/main/java/beans/backing/VacanteForm.java
@@ -2,6 +2,8 @@ package beans.backing;
 
 import beans.model.Candidato;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.apache.logging.log4j.LogManager;
@@ -15,17 +17,25 @@ public class VacanteForm {
     private Candidato candidato;
 
     Logger log = LogManager.getRootLogger();
-    
-    public VacanteForm(){
+
+    public VacanteForm() {
         log.info("Creado el objeto VacanteForm");
     }
-    
+
     public void setCandidato(Candidato candidato) {
         this.candidato = candidato;
     }
 
     public String enviar() {
         if (this.candidato.getNombre().equals("Brian")) {
+            if (this.candidato.getApellido().equals("Marquez")) {
+                String msg = "Gracias, pero Brian Marquez ya trabaj con nosotros";
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                String componentId = null; //mnsage global
+                facesContext.addMessage(componentId, facesMessage);
+                return "index";
+            }
             log.info("Entrando al caso de exito");
             return "exito";
         } else {
