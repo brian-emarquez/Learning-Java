17a3c03bb4bba33c7f758c0b6c533fcc02d13d6d
BrianMarquez3 <brian3marquez@gmail.com>
2021-10-08 18:19:35 -0500

    Value Changer Listener

---
+++ BolsaTrabajoV5/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,61 @@
+package beans.model;
+
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Named
+@RequestScoped
+public class Candidato {
+
+    private String nombre;
+    private String apellido;
+    private int salarioDeseado;
+    private Date FechaNacimiento;
+
+    Logger log = LogManager.getRootLogger();
+
+    public Candidato() {
+        log.info("Creando el objeto candidato");
+//      this.setNombre("Introduce tu nombre");
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        log.info("MOdificando la propiedad de nombre " + this.nombre);
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+        log.info("Modificacndo la propiedad de apellido " + this.apellido);
+    }
+
+    public int getSalarioDeseado() {
+        return salarioDeseado;
+    }
+
+    public void setSalarioDeseado(int salarioDeseado) {
+        this.salarioDeseado = salarioDeseado;
+        log.info("Modificacndo la propiedad de sueldoDeseado " + this.salarioDeseado);
+
+    }
+
+    public Date getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
+    }
+
+}
